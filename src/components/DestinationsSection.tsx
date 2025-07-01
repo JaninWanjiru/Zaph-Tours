ca0acaa64ef70dd819e2d48c9f18df043a5ddd78
JaninWanjiru <janinndongo02@gmail.com>
2025-07-01 23:26:54 +0300

    add and style rating component in testimonialsection

--- src/components/DestinationsSection.tsx
+++ src/components/DestinationsSection.tsx
@@ -14,43 +14,48 @@ const destinations = [
     name: "Mt.Kenya",
     image: "/Mt.Kenya.jpg",
     description: "Experience the majesty of Kenya's highest peak",
-    price: "Ksh.5500"
+    price: "Ksh.5500",
   },
   {
     name: "Mt.Ruwenzori",
     image: "/Mt.Rwenzori.jpg",
     description: "Explore the mystical Ruwenzori mountains",
-    price: "Ksh.5650"
+    price: "Ksh.5650",
   },
   {
     name: "Lake Nakuru",
     image: "/Lake Nakuru.jpg",
     description: "Famous for flamingos and rhinos with scenic beauty",
-    price: "Ksh.3400"
+    price: "Ksh.3400",
   },
   {
     name: "Tsavo National Park",
     image: "/Tsavo.jpeg",
-    description: "Kenya's largest park with a variety of landscapes and abundant wildlife",
-    price: "Ksh.4550"
+    description:
+      "Kenya's largest park with a variety of landscapes and abundant wildlife",
+    price: "Ksh.4550",
   },
   {
     name: "Nairobi National Park",
     image: "/Nairobi park.jpg",
     description: "A unique park to spot wildlife in the background",
-    price: "Ksh.4300"
+    price: "Ksh.4300",
   },
   {
     name: "Coastal Kenya",
     image: "/coastal.jpg",
-    description: "Relax along Kenya's coastline with pristine beaches and Swahili culture.",
-    price: "Ksh.5700"
+    description:
+      "Relax along Kenya's coastline with pristine beaches and Swahili culture.",
+    price: "Ksh.5700",
   },
 ];
 
 function DestinationsSection() {
   return (
-    <Box component="section" sx={{ bgcolor:'#F8F8F8', padding: { xs: 2, md: 4 } }}>
+    <Box
+      component="section"
+      sx={{ bgcolor: "#F8F8F8", padding: { xs: 2, md: 4 } }}
+    >
       <Typography
         variant="h4"
         fontWeight="bold"
@@ -63,24 +68,35 @@ function DestinationsSection() {
       <Grid container spacing={3}>
         {destinations.map((destination, idx) => (
           <Grid size={{ xs: 12, sm: 6, md: 4 }} key={idx}>
-          <Card sx={{ width: "300px" }}>
-            <CardMedia component="img" height="200px" image={destination.image} />
-            <CardContent >
-              <Typography variant="h6" color="primary">{destination.name}</Typography>
-              <Typography variant="body1">
-                {destination.description}
-              </Typography>
-              <Typography variant="subtitle2" color="primary">
+            <Card sx={{ width: "300px" }}>
+              <CardMedia
+                component="img"
+                height="200px"
+                image={destination.image}
+              />
+              <CardContent>
+                <Typography variant="h6" color="primary">
+                  {destination.name}
+                </Typography>
+                <Typography variant="body1">
+                  {destination.description}
+                </Typography>
+                <Typography variant="subtitle2" color="primary">
                   {destination.price}
                 </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" variant="contained" href="/destinations" sx={{borderRadius: '1rem'}}>
-                Explore Now
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+              </CardContent>
+              <CardActions>
+                <Button
+                  size="small"
+                  variant="contained"
+                  href="/destinations"
+                  sx={{ borderRadius: "1rem"}}
+                >
+                  Explore Now
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         ))}
       </Grid>
     </Box>
--- src/components/TestimonialsSection.tsx
+++ src/components/TestimonialsSection.tsx
@@ -1,35 +1,55 @@
-import { Box, Grid, Card, CardContent, Typography, Avatar, Stack } from "@mui/material";
-
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Avatar,
+  Stack,
+  Rating,
+} from "@mui/material";
 
 const testimonials = [
   {
     name: "Julius N.",
-    quote: "Zaph Tours gave me a life-changing safari experience. Everything was so smooth and well organized",
-    avatar: "https://randomuser.me/api/portraits/men/44.jpg"
+    quote:
+      "Zaph Tours gave me a life-changing safari experience. Everything was so smooth and well organized",
+    avatar: "https://randomuser.me/api/portraits/men/44.jpg",
+    rating: 5,
   },
   {
     name: "Margret N.",
-    quote: "The guides were friendly and professional. I saw more wildlife than I ever imagined. Highly recommended",
-    avatar: "https://randomuser.me/api/portraits/women/45.jpg"
+    quote:
+      "The guides were friendly and professional. I saw more wildlife than I ever imagined. Highly recommended",
+    avatar: "https://randomuser.me/api/portraits/women/45.jpg",
+    rating: 5,
   },
   {
     name: "Kelly M.",
-    quote: "Our family trip to Coastal Kenya was unforgettable. The beach resorts and cultural tours were amazing",
-    avatar: "https://randomuser.me/api/portraits/women/46.jpg"
-  }
+    quote:
+      "Our family trip to Coastal Kenya was unforgettable. The beach resorts and cultural tours were amazing",
+    avatar: "https://randomuser.me/api/portraits/women/46.jpg",
+    rating: 5,
+  },
 ];
 
 function TestimonialsSection() {
   return (
     <Box sx={{ padding: { xs: 2, md: 4 }, backgroundColor: "#f9f9f9" }}>
-      <Typography variant="h4" color='primary' textAlign="center" fontWeight="bold" gutterBottom>
+      <Typography
+        variant="h4"
+        color="primary"
+        textAlign="center"
+        fontWeight="bold"
+        padding="2rem"
+      >
         Our Travelers Remarks
       </Typography>
 
       <Grid container spacing={4} justifyContent="center">
         {testimonials.map((testimonial, idx) => (
           <Grid size={{ xs: 12, sm: 6, md: 4 }} key={idx}>
-            <Card sx={{ borderRadius: 3}}>
+            <Card sx={{ borderRadius: 3, height: "250px" }}>
               <CardContent>
                 <Stack direction="row" spacing={2} alignItems="center" mb={2}>
                   <Avatar src={testimonial.avatar} alt={testimonial.name} />
@@ -37,10 +57,13 @@ function TestimonialsSection() {
                     {testimonial.name}
                   </Typography>
                 </Stack>
-                <Typography variant="body2">
-                  “{testimonial.quote}”
-                </Typography>
+                <Typography variant="body1">“{testimonial.quote}”</Typography>
               </CardContent>
+              <Rating
+                value={testimonial.rating}
+                readOnly
+                sx={{ color: "secondary.main", padding: "0 1rem" }}
+              />
             </Card>
           </Grid>
         ))}
@@ -49,11 +72,4 @@ function TestimonialsSection() {
   );
 }
 
-export default TestimonialsSection
-
-
-
-
-
-
-
+export default TestimonialsSection;
