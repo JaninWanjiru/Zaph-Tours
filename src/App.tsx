d080d294f8d3ad662e5e6800a15e7431a6d515ad
JaninWanjiru <janinndongo02@gmail.com>
2025-07-01 16:02:33 +0300

    customize the theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import DestinationsPage from "./pages/DestinationsPage";
@@ -7,22 +8,37 @@ import ContactPage from "./pages/ContactPage";
 import Nav from "./components/Nav";
 import "./App.css";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#235339",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#97B067",
+      contrastText: "#fff",
+    },
+  },
+});
+
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/destinations" element={<DestinationsPage />} />
-        <Route path="/tripTypes" element={<TripTypesPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-      <footer>
-        Made with &hearts; by{" "}
-        <a href="https://github.com/JaninWanjiru">Jane Ndung'u</a>
-      </footer>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/destinations" element={<DestinationsPage />} />
+          <Route path="/tripTypes" element={<TripTypesPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+        <footer>
+          Made with &hearts; by{" "}
+          <a href="https://github.com/JaninWanjiru">Jane Ndung'u</a>
+        </footer>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
